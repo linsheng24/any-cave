4d091fd7fb80990d2643769d3a144f6081cd8bc4
linsheng24 <linsheng24@gmail.com>
2021-08-30 22:45:46 +0800

    feat: add openAPI

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,23 @@
-import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AppService } from './app.service';
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth/auth.service";
+import { LoginDto } from "./dtos";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiTags('auth')
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
-  async login(@Request() req) {
-    console.log(req.user)
-    return this.authService.login(req.user);
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 
+  @ApiTags('auth')
+  @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))
   @Post('auth/profile')
   async profile(@Request() req) {
