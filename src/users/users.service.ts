4787cd8ec7a7b7bac3eaa0d1f4ba3002d17497a4
linsheng24 <linsheng24@gmail.com>
2021-09-01 23:00:36 +0800

    feat: get user by user model

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import { RegistDto } from "../dtos";
 import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 import { UserDocument } from "../schemas/user.schema";
-import { Model } from 'mongoose';
 
 export type User = any;
 
@@ -10,28 +10,10 @@ export type User = any;
 export class UsersService {
   private readonly users: User[];
 
-  constructor(@InjectModel('User') private userModel: Model<UserDocument>) {
-    this.users = [
-      {
-        userId: 1,
-        username: 'john',
-        password: 'changeme',
-      },
-      {
-        userId: 2,
-        username: 'chris',
-        password: 'secret',
-      },
-      {
-        userId: 3,
-        username: 'maria',
-        password: 'guess',
-      },
-    ];
-  }
+  constructor(@InjectModel('User') private userModel: Model<UserDocument>) {}
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+    return await this.userModel.findOne({ username: username }).exec();
   }
 
   async createUser(registDto: RegistDto) {
